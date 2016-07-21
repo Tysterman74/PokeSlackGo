27c7104a6f8c9a462357417e3f743fc4e6a84f55
Tysterman <tynutsathitya@gmail.com>
2016-07-20 20:01:57 -0700

    Initial Commit of Files

---
+++ index.js
@@ -0,0 +1,22 @@
+var Slack = require('node-slack');
+var express = require('express');
+
+var slack = new Slack('https://hooks.slack.com/services/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', null);
+var app = express();
+
+app.get('/test', function (req, res) {
+    sendSlackMessage("Hallo");
+});
+
+app.listen(3000, function () {
+    console.log("listening.");
+});
+
+function sendSlackMessage(message) {
+    //var slack = new Slack('https://hooks.slack.com/services/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', null);
+    slack.send({
+        text: message,
+        channel: '#general',
+        username: 'TestBotTyler'
+    });
+}
