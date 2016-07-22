da30ccee707a65827cf7923da6d9116c819257e4
Tysterman <tynutsathitya@gmail.com>
2016-07-21 20:35:18 -0700

    More edits to get jsonParser working

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ var bodyParser = require('body-parser');
 var slack = new Slack('https://hooks.slack.com/services/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', null);
 var app = express();
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
+var jsonParser = bodyParser.json();
 
-app.post('/test', function (req, res) {
+app.post('/test', jsonParser, function (req, res) {
 
     //var reply = slack.respond(req.body, function (hook) {
     //
