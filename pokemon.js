01c0289630ce2edfc4d824740ce04d062d28ab57
Hans Marasigan <hansmarasigan@gmail.com>
2016-07-27 08:42:53 -0700

    trying test function

--- pokemon.js
+++ pokemon.js
@@ -1,10 +1,15 @@
 //making a pokemon file yay
 //it says pokemon ER NO it says pkmn
 
-module.exports = {
-	pkmn:function(){
-	console.log("woooh yeah pkmn");
+//module.exports = {
+//	pkmn:function(){
+//	console.log("woooh yeah pkmn");
 	
-	}
+//	}
 
+//}
+module.exports = {
+    test: function () {
+        console.log("test");
+    }
 }
