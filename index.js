60183ec138a1ea9c10b0a58a26435349300f40cf
Hans Marasigan <hansmarasigan@gmail.com>
2016-07-26 22:52:49 -0700

    adding in the exported function

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import "pokemon";
-
 var Slack = require('node-slack');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -7,6 +5,7 @@ var bodyParser = require('body-parser');
 var slack = new Slack('https://hooks.slack.com/services/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', null);
 var app = express();
 
+var pokedex = require('./pokemon');
 //app.configure(function () {
 //    app.use(bodyParser.json());    
 //})
@@ -23,7 +22,7 @@ app.post('/test', function (req, res) {
     var reply = slack.respond(req.body, function (hook) {
     
         console.log(hook);
-        //pkmn();
+        pokedex.pkmn();
     });
     
     res.json(reply);
