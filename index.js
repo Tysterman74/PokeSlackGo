054420d393f26528a879a555a3f57abeb4a31c95
Hans Marasigan <hansmarasigan@gmail.com>
2016-07-28 15:31:23 -0700

    pog champ.
    no change.

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/pokemon', function (req, res) {
         console.log("you are " + pkTest[1]);
         var pokeChoice = pkTest[1].toString();
         var pokeJudge = pokedex.pokeHammer(pokeChoice);
-        sendSlackMessage(pokeJudge.toString());
+        sendSlackMessage(pokeJudge);
     });
     
     //pokedex.test();
