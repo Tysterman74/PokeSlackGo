b1640bc500bbe65b97206ceee262c36479f4332f
Tyler Nutsathitya <tynutsathitya@gmail.com>
2016-07-28 06:44:05 -0700

    including comma where commas need to be

--- pokemon.js
+++ pokemon.js
@@ -11,7 +11,7 @@
 module.exports = {
     test: function () {
         console.log("test");
-    }
+    },
     pokeParse: function ( t1 ){
     	var parsed = t1.split(" ");
    	console.log(parsed);
