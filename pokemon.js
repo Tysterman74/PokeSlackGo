ca676f660f035d9add1bdc7f6450d638f3963878
Hans Marasigan <hansmarasigan@gmail.com>
2016-07-27 17:38:49 -0700

    pokeParse! testing

--- pokemon.js
+++ pokemon.js
@@ -12,4 +12,8 @@ module.exports = {
     test: function () {
         console.log("test");
     }
+    pokeParse: function ( t1 ){
+    	var parsed = t1.split(" ");
+    	console.log(parsed);
+    }
 }
