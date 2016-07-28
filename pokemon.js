3dcbd77503ed989e95bbdc8b2cccf2bc87183303
Hans Marasigan <hansmarasigan@gmail.com>
2016-07-28 13:32:40 -0700

    playing with cases.

--- pokemon.js
+++ pokemon.js
@@ -12,11 +12,24 @@ module.exports = {
     test: function () {
         console.log("test");
     },
+   
     pokeParse: function ( t1){
     	var lower = t1.toLowerCase();
     	var parsed =lower.split(" ");
    	return parsed;
    	
+   },
+   
+   pokeDef: function ( p1){
+   	if (p1[1].toString == "mystic"){
+   		return "YEAH GO MYSTIC";
+   	}
+   	else if (p1[1].toString == "valor"){
+   		return "team dumb";
+   	}
+   	else (p1[1].toString == "instinct"){
+   		return "You're okay";
+   	}
    }
    
 }
