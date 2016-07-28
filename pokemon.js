82b6d91649904c7b18cc05bf26a5d448c8807755
Hans Marasigan <hansmarasigan@gmail.com>
2016-07-28 13:21:26 -0700

    parsed back to an array

--- pokemon.js
+++ pokemon.js
@@ -15,8 +15,8 @@ module.exports = {
     pokeParse: function ( t1){
     	var lower = t1.toLowerCase();
     	var parsed =lower.split(" ");
-   	//return parsed;
-   	return '';
+   	return parsed;
+   	
    }
    
 }
