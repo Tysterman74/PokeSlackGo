89e4f710dd126037446f623b1821ba8f2d3d05a3
Hans Marasigan <hansmarasigan@gmail.com>
2016-07-28 14:14:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/pokemon', function (req, res) {
         var pkTest = pokedex.pokeParse(hook.text);
         console.log("you are " + pkTest[1]);
         var pokeChoice = pkTest[1].toString();
-        var pokeJudge = pokeDef(pokeChoice);
+        var pokeJudge = pokeHammer(pokeChoice);
         sendSlackMessage(pokeJudge);
     });
     
