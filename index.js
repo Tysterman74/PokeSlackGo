52e6b959d3ca9fec051184ef68bc815adec649d9
Hans Marasigan <hansmarasigan@gmail.com>
2016-07-28 12:17:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ app.post('/test', function (req, res) {
 
 app.post('/pokemon', function (req, res) {
     var reply = slack.respond(req.body, function (hook) {
-        
+        var pkTest = pokedex.pokeParse(hook.text)
         //console.log(hook);
-        sendSlackMessage(pokedex.pokeParse(hook.text));
+        sendSlackMessage(pkTest);
     });
     
     //pokedex.test();
