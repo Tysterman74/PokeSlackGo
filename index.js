67d86e7241217689c183ec44cb02f8648207f422
Jeffrey Gracia <jgracia@bdi.int>
2016-08-02 10:19:46 -0700

    #general vs general

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ Query Location
 */
 
 bender.on('start',function() {
-	bender.postMessageToChannel('#general', 'Bite my shiny metal ass!');
+	bender.postMessageToChannel('general', 'Bite my shiny metal ass!');
 };
 
 app.post('/test', function (req, res) {
