5c08d08430d9370d4f9845b1c62cd82745fab0e1
Jeffrey Gracia <jgracia@bdi.int>
2016-08-03 10:28:30 -0700

    quick test

--- bender.js
+++ bender.js
@@ -45,7 +45,7 @@ Bender.prototype._replyToHuman = function( originalMessage ) {
 	console.log("OG: " + originalMessage.channel);
 	console.log("Channel: " + channel);
 	console.log("Channel Name: " + channel.name);
-	self.postMessageToChannel(originalMessage.channel, 'Hey ' + originalMessage.user + ', Bite my shiny metal ass!', {as_user: true});
+	self.postMessageToChannel('general', 'Hey ' + originalMessage.user + ', Bite my shiny metal ass!', {as_user: true});
 };
 
 Bender.prototype._getChannelByID = function( channelID ) {
