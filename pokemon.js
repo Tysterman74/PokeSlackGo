7ec03bc032873cc585dc8442695cc324b8b29d0d
Hans Marasigan <hansmarasigan@gmail.com>
2016-08-15 12:00:57 -0700

    adding a bit of error finding.

--- pokemon.js
+++ pokemon.js
@@ -69,6 +69,12 @@ function pokeHammer (p1, l1, callback){
    		
    	}
    	else if (p1 === "addlocation"){
+   		if (l1.length()<4)
+   		{
+   			callback("Not enough information! you maybe missing your lat longs")
+   		}
+   		else
+   		
    		dbList.push(l1[2],l1[3],l1[4]);
    		console.log("you're passing location" + dbList);
    		
