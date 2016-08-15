0c23fa744aa680e84333ce3aa99fc2ae37f6b3ad
Hans Marasigan <hansmarasigan@gmail.com>
2016-08-15 12:48:01 -0700

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -47,7 +47,7 @@ function pokeHammer (p1, l1, callback){
    	var dbList=[];
    	switch(p1){
    	case "query":
-   		if (l1.length<4){
+   		if (l1.length<5){
    			callback("You are missing some information here! Please check you put: \n location name latitude longitude");
    			break;
    		}
