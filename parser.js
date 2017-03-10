92fe420de5a19fe4b899b45c1a75fb024711d2be
HMCodingSnowMan <hansmarasigan@gmail.com>
2017-03-09 17:03:04 -0800

    removed debug console log

--- parser.js
+++ parser.js
@@ -20,16 +20,12 @@ var parsed;
 
 
 function init(){
-	console.log("BayMax is listening!");
-	console.log("I will be taking your orders");
+	console.log("BayMax Parser is listening!");
+	//console.log("I will be parsing ");
 }//end init
 
 function fullParse(input){
 	this.parsed = input.split(" ");
-	console.log("I've split the line");
-	console.log("It says " + this.parsed);
-	//console.log("length of parsed is " + this.parsed.length);
-	parsePrinter();
 }//end fullParse
 
 function parsePrinter(){
@@ -39,7 +35,7 @@ function parsePrinter(){
 		console.log(this.parsed[i]);
 		i++;
 	}
-}
+}//end parsePrinter
 
 
 
