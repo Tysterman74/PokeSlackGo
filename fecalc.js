5b997e207541cb87ff63d678f4c9f62437d15a39
Jeff Gracia <jeff.c.gracia@gmail.com>
2017-03-15 21:15:26 -0700

    Finished Unit Tests for feCreateChar  and feParse

--- fecalc.js
+++ fecalc.js
@@ -1,5 +1,6 @@
 //FE:H Custom Calc
 var math = require('mathjs');
+var _und = require('underscore');
 
 module.exports = {
     init() {
@@ -22,6 +23,9 @@ function init() {
     console.log("\nFECalc");
     console.log('testing feParse');
     feParseTest();
+    console.log('\ntesting feCreateChar');
+    feCharTest();
+    console.log("\nFinished Tests");
 }
 
 //feCustomCalc takes data and throws back an endmessage 
@@ -60,8 +64,8 @@ function feParse( data ) {
 
 // feCreateChar creates objects for the CustomCalc
 // technically a parser*
-// hp, atk, spd, def, res, [(d)braveweapon, (e)ffective, r(a)nged, (m)agic,
-//     c(o)unterable, (r)ed, (b)lue, (g)reen, c(o)lorless, ad(v)antage]
+// hp, atk, spd, def, res, [(d)braveweapon, (e)ffective, ra(n)ged, (m)agic,
+//     c(o)unterable, (r)ed, (b)lue, (g)reen, (c)olorless, (a)dvantage]
 function feCreateChar ( parseddata ) {
     var chars = parseddata; 
     //for loop for each in array
@@ -78,24 +82,27 @@ function feCreateChar ( parseddata ) {
                 flags.push(chars[i][j].toString());
             }
         }
-        for (var g in num) {
-             
-        }
         var dummy = {hp:math.floor((num / 100000000)), atk:math.floor((num / 1000000) % 100), spd: math.floor((num / 10000) % 100), def: math.floor((num / 100) % 100), res: math.floor(num % 100), flags:flags};
         chars[i] = dummy;
     }
     return chars;
 }
 
+// feParseTest is a Custom Unit Test for function feParse()
 function feParseTest () {
     var results = { total: 0, bad: 0};
     
     function test( ran, expected ) {
        results.total++;
        var running = feParse(ran);
-       if ( running !== expected ) {
-          results.bad++; 
-          console.log( "Expected " + expected + ", but returned " + running);
+    //    console.log( running );
+       for ( var i = 0; i < running.length; i++ ){
+           if ( running[i] != expected[i])
+           {
+              results.bad++; 
+              console.log( "Expected " + expected + " but returned " + running );
+              break; 
+           }
        }
     }
 
@@ -104,7 +111,37 @@ function feParseTest () {
     test( "gn3849261735vb3035263024", ["gn3849261735", "b3035263024",] );
     test( "g3849261735nvsb3035263024", ["g3849261735n", "b3035263024",] );
     test( "g3849261735nv3035263024b", ["g3849261735n", "3035263024b",] );
+    test( "g 38 49 26 17 35 n v 30 35 26 30 24 b", ["g3849261735n", "3035263024b",] );
+    test( "g 38 49 26 17 35 n vs 30 35 26 30 24 b", ["g3849261735n", "3035263024b",] );
+    test( "g n 38 49 26 17 35  vs b 30 35 26 30 24", ["gn3849261735", "b3035263024",] );
     test( "3849261735v3035263024", ["3849261735", "3035263024",] );
+    console.log( results.total - results.bad + " passed. " + results.bad + " failed out of " + results.total);
+    
+}
+
+// feCharTest is a Custom Unit Test for function feCreateChar()
+function feCharTest () {
+    var results = { total: 0, bad: 0};
+    
+    function test( ran, expected ) {
+       results.total++;
+       var running = feCreateChar(feParse(ran));
+    //    console.log( running );
+    // had to use underscore.js for the complex equivalence
+       if ( !_und.isEqual(running, expected) ) {
+              results.bad++; 
+              console.log( "Expected " + expected + " but returned " + running );
+           }
+       }
+    
+    test( "3849261735gnvs3035263024b", [ { hp: 38, atk: 49, spd: 26, def: 17, res: 35, flags: [ 'g', 'n' ] }, { hp: 30, atk: 35, spd: 26, def: 30, res: 24, flags: [ 'b' ] } ]);
+    test( "3849261735gnv3035263024b", [ { hp: 38, atk: 49, spd: 26, def: 17, res: 35, flags: [ 'g', 'n' ] }, { hp: 30, atk: 35, spd: 26, def: 30, res: 24, flags: [ 'b' ] } ] );
+    test( "gn3849261735vb3035263024", [ { hp: 38, atk: 49, spd: 26, def: 17, res: 35, flags: [ 'g', 'n' ] }, { hp: 30, atk: 35, spd: 26, def: 30, res: 24, flags: [ 'b' ] } ] );
+    test( "g3849261735nvsb3035263024", [ { hp: 38, atk: 49, spd: 26, def: 17, res: 35, flags: [ 'g', 'n' ] }, { hp: 30, atk: 35, spd: 26, def: 30, res: 24, flags: [ 'b' ] } ] );
+    test( "g3849261735nv3035263024b", [ { hp: 38, atk: 49, spd: 26, def: 17, res: 35, flags: [ 'g', 'n' ] }, { hp: 30, atk: 35, spd: 26, def: 30, res: 24, flags: [ 'b' ] } ] );
+    test( "g 38 49 26 17 35 n v 30 35 26 30 24 b", [ { hp: 38, atk: 49, spd: 26, def: 17, res: 35, flags: [ 'g', 'n' ] }, { hp: 30, atk: 35, spd: 26, def: 30, res: 24, flags: [ 'b' ] } ] );
+    test( "g 38 49 26 17 35 n vs 30 35 26 30 24 b", [ { hp: 38, atk: 49, spd: 26, def: 17, res: 35, flags: [ 'g', 'n' ] }, { hp: 30, atk: 35, spd: 26, def: 30, res: 24, flags: [ 'b' ] } ] );
+    test( "g n 38 49 26 17 35  vs b 30 35 26 30 24", [ { hp: 38, atk: 49, spd: 26, def: 17, res: 35, flags: [ 'g', 'n' ] }, { hp: 30, atk: 35, spd: 26, def: 30, res: 24, flags: [ 'b' ] } ] );
     console.log( results.total - results.bad + " passed. " + results.bad + " failed out of " + results.total);
     
 }
