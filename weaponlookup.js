f05bac9d9c3a77d57dfe407b186f9538b2dcc44c
HMCodingSnowMan <hansmarasigan@gmail.com>
2017-03-24 18:26:36 -0700

    starting weaponlookup

--- weaponlookup.js
+++ weaponlookup.js
@@ -1,30 +1,19 @@
 //making a weapon look up class for fe-heroes.
 //similar to the charlookup so gonna try to inherit everything from it
+var cLook=require('./characterlookup'); 
+
 
 module.exports = {
 	init(){
 		init();
-	},
-	
-	inherits (){
-		inherits();
 	}
-		
+	lookUp(object, callback){
+		return lookUp(object, callback)
+	}
 	
 
 }
 
-function inherits(ctor, supterCtor){
-	ctor.supe_=superCtor;
-	ctor.prototype = Object.create(superCtor.prototype, {
-		constructor:{
-			value:ctor,
-			enumerable:false,
-			writable:true,
-			configurable:true
-		}
-	});
-};
 function init(){
 	console.log("I have been awakened");
 }
