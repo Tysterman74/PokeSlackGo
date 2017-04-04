59799eb8efbe950a27e9818e0476f7e42d7e2697
HMCodingSnowMan <hansmarasigan@gmail.com>
2017-04-03 20:13:44 -0700

    added lookup

--- weaponlookup.js
+++ weaponlookup.js
@@ -25,5 +25,29 @@ function init(database){
 }
 
 function lookUp(object, callback){
-	console.log("hi");
+	console.log("I will begin your weapon query");
+	db.queryCharacter(object, function(message){
+	if (message!="DNE")
+	{
+		var toReturn ={
+			text:"Here's what the weapon I found!"
+			+message[0].name;
+			
+		}//end toReturn
+	}//end if message=1
+	else{
+		var toReturn={
+			text:"there are " + message.length + " possible weapons."
+		}//end toReturn
+	}//end else
+	callback(toReturn);
+	}//end if message!=DNE
+	else{
+		var toReturn={
+			text: "i couldn't find " + object + ". please check your spelling and try again."
+		}//end to return
+	callback(toReturn);
+	}//end else
+
+});
 }
