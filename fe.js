7bc894046e8f3c37e7b7a63063316aa1b988b200
HMCodingSnowMan <hansmarasigan@gmail.com>
2017-04-08 10:52:44 -0700

    added weapon to api flow

--- fe.js
+++ fe.js
@@ -1,6 +1,6 @@
 module.exports = {
-    init(characterLookup) {
-        init(characterLookup);
+    init(characterLookup,weaponLookup) {
+        init(characterLookup,weaponLookup);
     },
     execute(parsedLine, callback) {
         execute(parsedLine, callback);
@@ -8,13 +8,16 @@ module.exports = {
 }
 
 var characterLookupObj;
+var weaponLookupObj;
 
 var commandEnums = {
     CHARACTERLOOKUP: 'CHARACTER'
+	WEAPONLOOKUP: 'WEAPON'
 }
 
-function init(characterLookup) {
+function init(characterLookup,weaponLookup) {
     characterLookupObj = characterLookup;
+	weaponLookupObj = weaponLookup;
 }
 
 function execute(parsedLine, callback) {
@@ -24,6 +27,9 @@ function execute(parsedLine, callback) {
         case commandEnums.CHARACTERLOOKUP:
             characterLookup(parsedLine, callback);
             break;
+		case commandEnums.WEAPONLOOKUP:
+			weaponLookup(parsedLine, callback);
+			break;
         default:
             break;
     }
@@ -34,4 +40,11 @@ function characterLookup(parsedLine, callback) {
     characterLookupObj.lookUp(name, function (result) {
         callback(result);
     });
+}
+
+function weaponLookup(parsedLine, callback){
+	var name = parsedLine.data;
+	weaponLookupObj.lookUp(name,function(result)){
+		callback(result);
+	});
 }
