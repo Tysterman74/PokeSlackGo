4dc5fac50c515c988732390d07f3deea5f8ccc00
HMCodingSnowMan <hansmarasigan@gmail.com>
2017-04-08 10:58:14 -0700

    init fe

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ logger.init(database);
 parser.init();
 cLookUp.init(database);
 vLookUp.init(database);
+fe.init(cLookup,vLookup);
 
 /*database.getLogs(function (result) {
     console.log(result);
